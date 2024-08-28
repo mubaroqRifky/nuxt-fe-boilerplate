aeac2ec002ff2a5a4df25167e874db19299874ff
mubaroqRifky <mubaroq.rifki92@gmail.com>
2024-08-28 14:35:18 +0700

    add form login

--- pages/login.vue
+++ pages/login.vue
@@ -9,22 +9,49 @@
                 <h1 class="font-bold text-2xl text-gray-dark uppercase">
                     Login
                 </h1>
-                <div class="text-black flex justify-center">
-                    <button
-                        class="google-btn whitespace-nowrap max-w-xs"
-                        alt="Sign In"
-                        @click="googleLoginHandler"
-                    >
-                        <IconGoogle width="30px" height="30px" />
-                    </button>
 
-                    <ButtonGoogle
-                        ref="btnGoogle"
-                        class="hidden"
-                        :params="params"
-                        :renderParams="renderParams"
-                        :success="onsuccess"
-                    />
+                <div class="flex flex-col gap-6 w-full">
+                    <div class="mb-6 flex flex-col gap-3">
+                        <InputText
+                            theme="default"
+                            placeholder="Enter Your Email"
+                            v-model="form.email"
+                            v-model:error="errors.email"
+                        />
+                        <InputPassword
+                            theme="default"
+                            placeholder="Password"
+                            v-model="form.password"
+                            v-model:error="errors.password"
+                            @keyup.enter="handleLogin"
+                        />
+                    </div>
+
+                    <div class="flex flex-col">
+                        <ButtonMain text="Masuk" @press="handleLogin" />
+                    </div>
+
+                    <p class="text-xs text-center leading-4 -mb-2 font-medium">
+                        atau masuk dengan
+                    </p>
+
+                    <div class="text-black flex justify-center">
+                        <button
+                            class="google-btn whitespace-nowrap max-w-xs"
+                            alt="Sign In"
+                            @click="googleLoginHandler"
+                        >
+                            <IconGoogle width="30px" height="30px" />
+                        </button>
+
+                        <ButtonGoogle
+                            ref="btnGoogle"
+                            class="hidden"
+                            :params="params"
+                            :renderParams="renderParams"
+                            :success="onsuccess"
+                        />
+                    </div>
                 </div>
             </div>
         </section>
@@ -45,6 +72,7 @@ const router = useRouter();
 const user = useUserStore();
 
 const btnGoogle = ref(null);
+const form = reactive({ email: "" || "", password: "" });
 const params = ref({ client_id: $config.public.GOOGLE_OAUTH_CLIENT_ID });
 const renderParams = ref({ width: 250, height: 50, longtitle: true });
 
@@ -63,6 +91,18 @@ const onsuccess = async (response) => {
         loadingStart();
         const id_token = response.credential;
         const { data } = await $auth.loginGoogle(id_token);
+        signInUser(data.token);
+    } catch (error) {
+        throw new ErrorHandler(error);
+    } finally {
+        loadingStop();
+    }
+};
+
+const handleLogin = async () => {
+    try {
+        loadingStart();
+        const { data } = await $auth.login(form);
         signInUser(data.token);
     } catch (error) {
         throw new ErrorHandler(error);
