188543524e432e0ae72b88b71c4db1dd0b38df0a
mubaroqRifky <mubaroq.rifki92@gmail.com>
2025-08-01 09:52:55 +0700

    create modal bottom form & modify modal bototm

--- components/modal/ModalBottom.vue
+++ components/modal/ModalBottom.vue
@@ -1,6 +1,5 @@
 <template lang="">
     <section
-        ref="modalContainer"
         class="fixed top-0 left-0 right-0 bottom-0 bg-transparent flex flex-col justify-end items-center z-50 mobile-width-constraint"
         @click.self.stop="closeHandler"
     >
@@ -10,6 +9,7 @@
                 class="bg-white rounded-t-xl w-full px-6 py-6 grid gap-4 content-start text-center shadow_up relative max-w-md"
             >
                 <button
+                    ref="buttonStackModal"
                     class="absolute -top-12 right-2 bg-white rounded-full p-2 z-50"
                     @click="closeHandler"
                 >
@@ -105,7 +105,7 @@ const props = defineProps({
 
 const emit = defineEmits(["close", "proses"]);
 const bodyShow = ref(false);
-const modalContainer = ref(false);
+const buttonStackModal = ref(false);
 
 const MODAL = {
     SUCCESS: "success",
@@ -136,13 +136,18 @@ const actionHandler = () => {
     emit("proses");
 };
 
-onMounted(() => {
-    document.body.addEventListener("keydown", closeModal);
-    bodyShow.value = true;
+const addStackModalClass = () => {
     const stackModal = [MODAL.SUCCESS, MODAL.ERROR, MODAL.WARNING];
     if (!stackModal.includes(props.type)) {
-        modalContainer.value.classList.add("stack-modal");
+        buttonStackModal.value.classList.add("stack-modal");
     }
+};
+
+onMounted(async () => {
+    document.body.addEventListener("keydown", closeModal);
+    bodyShow.value = true;
+    await nextTick();
+    addStackModalClass();
 });
 </script>
 
